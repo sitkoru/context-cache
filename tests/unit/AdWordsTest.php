6f9d38923cd53c63b0a7e74ed551fe1208ce0aa3
SonicGD <sonicgd@gmail.com>
2017-03-06 18:32:50 +0500

    Add tests

---
+++ tests/unit/AdWordsTest.php
@@ -0,0 +1,159 @@
+<?php
+declare(strict_types=1);
+
+namespace sitkoru\contextcache\tests\unit;
+
+
+use Google\AdsApi\AdWords\v201702\cm\AdGroup;
+use Google\AdsApi\AdWords\v201702\cm\AdGroupAd;
+use Google\AdsApi\AdWords\v201702\cm\AdGroupCriterion;
+use Google\AdsApi\AdWords\v201702\cm\Campaign;
+use PHPUnit\Framework\TestCase;
+use sitkoru\contextcache\adwords\AdWordsAdGroupCriterionsProvider;
+use sitkoru\contextcache\adwords\AdWordsAdGroupsProvider;
+use sitkoru\contextcache\adwords\AdWordsAdsProvider;
+use sitkoru\contextcache\adwords\AdWordsCampaignsProvider;
+use sitkoru\contextcache\common\cache\MongoDbCacheProvider;
+use sitkoru\contextcache\ContextEntitiesProvider;
+
+class AdWordsTest extends TestCase
+{
+    private $provider;
+
+
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $cacheProvider = new MongoDbCacheProvider('mongodb://mongodb');
+        $contextEntitiesProvider = new ContextEntitiesProvider($cacheProvider);
+        $this->provider = $contextEntitiesProvider->getAdWordsProvider(ADWORDS_CUSTOMER_ID, ADWORDS_AUTH_FILE_PATH);
+    }
+
+    /**
+     * @dataProvider cacheProvider
+     * @param bool $clearCache
+     */
+    public function testLoadCampaigns(bool $clearCache): void
+    {
+        $this->assertInstanceOf(AdWordsCampaignsProvider::class, $this->provider->campaigns);
+        if ($clearCache) {
+            $this->provider->campaigns->clearCache();
+        }
+        $campaigns = $this->provider->campaigns->getAll([ADWCampaignId]);
+        $this->assertCount(1, $campaigns);
+        foreach ($campaigns as $campaign) {
+            $this->assertInstanceOf(Campaign::class, $campaign);
+        }
+    }
+
+    /**
+     * @dataProvider cacheProvider
+     * @param bool $clearCache
+     */
+    public function testLoadGroups(bool $clearCache): void
+    {
+        $this->assertInstanceOf(AdWordsAdGroupsProvider::class, $this->provider->adGroups);
+        if ($clearCache) {
+            $this->provider->adGroups->clearCache();
+        }
+        $adGroups = $this->provider->adGroups->getAll([ADWAdGroupId]);
+        $this->assertCount(1, $adGroups);
+        foreach ($adGroups as $adGroup) {
+            $this->assertInstanceOf(AdGroup::class, $adGroup);
+        }
+    }
+
+    /**
+     * @dataProvider cacheProvider
+     * @param bool $clearCache
+     */
+    public function testLoadGroupsByCampaignIds($clearCache): void
+    {
+        $this->assertInstanceOf(AdWordsAdGroupsProvider::class, $this->provider->adGroups);
+        if ($clearCache) {
+            $this->provider->adGroups->clearCache();
+        }
+        $adGroups = $this->provider->adGroups->getByCampaignIds([ADWCampaignId]);
+        $this->assertTrue(count($adGroups) > 0);
+        foreach ($adGroups as $adGroup) {
+            $this->assertInstanceOf(AdGroup::class, $adGroup);
+        }
+    }
+
+    /**
+     * @dataProvider cacheProvider
+     * @param bool $clearCache
+     */
+    public function testLoadAds($clearCache): void
+    {
+        $this->assertInstanceOf(AdWordsAdsProvider::class, $this->provider->ads);
+        if ($clearCache) {
+            $this->provider->ads->clearCache();
+        }
+        $ads = $this->provider->ads->getAll([ADWAdId]);
+        $this->assertCount(1, $ads);
+        foreach ($ads as $ad) {
+            $this->assertInstanceOf(AdGroupAd::class, $ad);
+        }
+    }
+
+    /**
+     * @dataProvider cacheProvider
+     * @param bool $clearCache
+     */
+    public function testLoadAdsByGroupIds($clearCache): void
+    {
+        $this->assertInstanceOf(AdWordsAdsProvider::class, $this->provider->ads);
+        if ($clearCache) {
+            $this->provider->ads->clearCache();
+        }
+        $ads = $this->provider->ads->getByAdGroupIds([ADWAdGroupId]);
+        $this->assertTrue(count($ads) > 0);
+        foreach ($ads as $ad) {
+            $this->assertInstanceOf(AdGroupAd::class, $ad);
+        }
+    }
+
+    /**
+     * @dataProvider cacheProvider
+     * @param bool $clearCache
+     */
+    public function testLoadCriterions($clearCache): void
+    {
+        $this->assertInstanceOf(AdWordsAdGroupCriterionsProvider::class, $this->provider->criterions);
+        if ($clearCache) {
+            $this->provider->criterions->clearCache();
+        }
+        $criterions = $this->provider->criterions->getAll([ADWCriterionId]);
+        $this->assertCount(1, $criterions);
+        foreach ($criterions as $criterion) {
+            $this->assertInstanceOf(AdGroupCriterion::class, $criterion);
+        }
+    }
+
+    /**
+     * @dataProvider cacheProvider
+     * @param bool $clearCache
+     */
+    public function testLoadCriterionsByGroupIds($clearCache): void
+    {
+        $this->assertInstanceOf(AdWordsAdGroupCriterionsProvider::class, $this->provider->criterions);
+        if ($clearCache) {
+            $this->provider->criterions->clearCache();
+        }
+        $criterions = $this->provider->criterions->getByAdGroupIds([ADWAdGroupId]);
+        $this->assertTrue(count($criterions) > 0);
+        foreach ($criterions as $criterion) {
+            $this->assertInstanceOf(AdGroupCriterion::class, $criterion);
+        }
+    }
+
+    public function cacheProvider()
+    {
+        return [
+            [true],
+            [false]
+        ];
+    }
+}
---
+++ tests/unit/DirectTest.php
@@ -0,0 +1,157 @@
+<?php
+declare(strict_types=1);
+
+namespace sitkoru\contextcache\tests\unit;
+
+
+use directapi\services\adgroups\models\AdGroupGetItem;
+use directapi\services\ads\models\AdGetItem;
+use directapi\services\campaigns\models\CampaignGetItem;
+use directapi\services\keywords\models\KeywordGetItem;
+use PHPUnit\Framework\TestCase;
+use sitkoru\contextcache\common\cache\MongoDbCacheProvider;
+use sitkoru\contextcache\ContextEntitiesProvider;
+use sitkoru\contextcache\direct\DirectAdGroupsProvider;
+use sitkoru\contextcache\direct\DirectAdsProvider;
+use sitkoru\contextcache\direct\DirectCampaignsProvider;
+use sitkoru\contextcache\direct\DirectKeywordsProvider;
+
+class DirectTest extends TestCase
+{
+    private $provider;
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $cacheProvider = new MongoDbCacheProvider('mongodb://mongodb');
+        $contextEntitiesProvider = new ContextEntitiesProvider($cacheProvider);
+        $this->provider = $contextEntitiesProvider->getDirectProvider(DIRECT_ACCESS_TOKEN, DIRECT_LOGIN);
+    }
+
+    /**
+     * @dataProvider cacheProvider
+     * @param bool $clearCache
+     */
+    public function testLoadCampaigns(bool $clearCache): void
+    {
+        $this->assertInstanceOf(DirectCampaignsProvider::class, $this->provider->campaigns);
+        if ($clearCache) {
+            $this->provider->campaigns->clearCache();
+        }
+        $campaigns = $this->provider->campaigns->getAll([YDCampaignId]);
+        $this->assertCount(1, $campaigns);
+        foreach ($campaigns as $campaign) {
+            $this->assertInstanceOf(CampaignGetItem::class, $campaign);
+        }
+    }
+
+    /**
+     * @dataProvider cacheProvider
+     * @param bool $clearCache
+     */
+    public function testLoadGroups(bool $clearCache): void
+    {
+        $this->assertInstanceOf(DirectAdGroupsProvider::class, $this->provider->adGroups);
+        if ($clearCache) {
+            $this->provider->adGroups->clearCache();
+        }
+        $adGroups = $this->provider->adGroups->getAll([YDAdGroupId]);
+        $this->assertCount(1, $adGroups);
+        foreach ($adGroups as $adGroup) {
+            $this->assertInstanceOf(AdGroupGetItem::class, $adGroup);
+        }
+    }
+
+    /**
+     * @dataProvider cacheProvider
+     * @param bool $clearCache
+     */
+    public function testLoadGroupsByCampaignIds($clearCache): void
+    {
+        $this->assertInstanceOf(DirectAdGroupsProvider::class, $this->provider->adGroups);
+        if ($clearCache) {
+            $this->provider->adGroups->clearCache();
+        }
+        $adGroups = $this->provider->adGroups->getByCampaignIds([YDCampaignId]);
+        $this->assertTrue(count($adGroups) > 0);
+        foreach ($adGroups as $adGroup) {
+            $this->assertInstanceOf(AdGroupGetItem::class, $adGroup);
+        }
+    }
+
+    /**
+     * @dataProvider cacheProvider
+     * @param bool $clearCache
+     */
+    public function testLoadAds($clearCache): void
+    {
+        $this->assertInstanceOf(DirectAdsProvider::class, $this->provider->ads);
+        if ($clearCache) {
+            $this->provider->ads->clearCache();
+        }
+        $ads = $this->provider->ads->getAll([YDAdId]);
+        $this->assertCount(1, $ads);
+        foreach ($ads as $ad) {
+            $this->assertInstanceOf(AdGetItem::class, $ad);
+        }
+    }
+
+    /**
+     * @dataProvider cacheProvider
+     * @param bool $clearCache
+     */
+    public function testLoadAdsByGroupIds($clearCache): void
+    {
+        $this->assertInstanceOf(DirectAdsProvider::class, $this->provider->ads);
+        if ($clearCache) {
+            $this->provider->ads->clearCache();
+        }
+        $ads = $this->provider->ads->getByAdGroupIds([YDAdGroupId]);
+        $this->assertTrue(count($ads) > 0);
+        foreach ($ads as $ad) {
+            $this->assertInstanceOf(AdGetItem::class, $ad);
+        }
+    }
+
+    /**
+     * @dataProvider cacheProvider
+     * @param bool $clearCache
+     */
+    public function testLoadCriterions($clearCache): void
+    {
+        $this->assertInstanceOf(DirectKeywordsProvider::class, $this->provider->keywords);
+        if ($clearCache) {
+            $this->provider->keywords->clearCache();
+        }
+        $keywords = $this->provider->keywords->getAll([YDCriterionId]);
+        $this->assertCount(1, $keywords);
+        foreach ($keywords as $keyword) {
+            $this->assertInstanceOf(KeywordGetItem::class, $keyword);
+        }
+    }
+
+    /**
+     * @dataProvider cacheProvider
+     * @param bool $clearCache
+     */
+    public function testLoadCriterionsByGroupIds($clearCache): void
+    {
+        $this->assertInstanceOf(DirectKeywordsProvider::class, $this->provider->keywords);
+        if ($clearCache) {
+            $this->provider->keywords->clearCache();
+        }
+        $keywords = $this->provider->keywords->getByAdGroupIds([YDAdGroupId]);
+        $this->assertTrue(count($keywords) > 0);
+        foreach ($keywords as $keyword) {
+            $this->assertInstanceOf(KeywordGetItem::class, $keyword);
+        }
+    }
+
+    public function cacheProvider()
+    {
+        return [
+            [true],
+            [false]
+        ];
+    }
+}
