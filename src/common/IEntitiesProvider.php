019cb630d90368c6dcb06d2316fa00dc8382e6d3
SonicGD <sonicgd@gmail.com>
2017-03-16 10:50:54 +0500

    Update should return UpdateResult

--- src/common/IEntitiesProvider.php
+++ src/common/IEntitiesProvider.php
@@ -3,11 +3,13 @@
 namespace sitkoru\contextcache\common;
 
 
+use sitkoru\contextcache\common\models\UpdateResult;
+
 interface IEntitiesProvider
 {
     public function getAll(array $ids): array;
 
     public function getOne($id);
 
-    public function update(array $entities): bool;
+    public function update(array $entities): UpdateResult;
 }
---
+++ src/common/models/UpdateResult.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace sitkoru\contextcache\common\models;
+
+
+class UpdateResult
+{
+    /**
+     * @var bool
+     */
+    public $success = true;
+
+    /**
+     * @var array
+     */
+    public $errors = [];
+}
